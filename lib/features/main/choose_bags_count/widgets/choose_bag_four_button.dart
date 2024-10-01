00149c6b8707e8fe7e679c49a5978aad63510faa
rover <kirill2142k@gmail.com>
2024-10-01 20:49:36 +0300

    add comment

--- lib/features/main/choose_bags_count/widgets/choose_bag_four_button.dart
+++ lib/features/main/choose_bags_count/widgets/choose_bag_four_button.dart
@@ -4,7 +4,7 @@ class ChooseBagFourButton extends StatelessWidget {
   const ChooseBagFourButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //center
     return Center(
       child: Row(
         children: [
